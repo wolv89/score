d5efefea39312388cdb864e25e0d67a5338bc0dc
Pete <peter.delafosse@gmail.com>
2026-02-20 00:02:52 +1100

    Starting to build out the scoring mechanics

--- scoring.go
+++ scoring.go
@@ -13,10 +13,19 @@ type LiveSet uint32
 const (
 	POINT_LEFT  = LiveSet(1 << 8)
 	POINT_RIGHT = LiveSet(1)
+	POINT_RESET = 0xFFFF0000
 
 	GAME_LEFT  = LiveSet(1 << 20)
 	GAME_RIGHT = LiveSet(1 << 16)
 )
+
+var Scores = [5]string{
+	" 0",
+	"15",
+	"30",
+	"40",
+	"Ad",
+}
 
 func (ls LiveSet) PointLeft() LiveSet {
 
@@ -33,16 +42,30 @@ func (ls LiveSet) PointRight() LiveSet {
 }
 
 func (ls LiveSet) GameLeft() LiveSet {
-
-	ls += GAME_LEFT
-	return ls
-
+	return (ls & POINT_RESET) + GAME_LEFT
 }
 
 func (ls LiveSet) GameRight() LiveSet {
+	return (ls & POINT_RESET) + GAME_RIGHT
+}
 
-	ls += GAME_RIGHT
-	return ls
+func (ls LiveSet) Score() string {
+
+	scoreLeft := uint8(ls >> 8)
+	scoreRight := uint8(ls)
+
+	var b strings.Builder
+	b.Grow(5)
+
+	if scoreLeft >= 4 {
+		b.WriteString("Ad-  ")
+	} else if scoreRight >= 4 {
+		b.WriteString("  -Ad")
+	} else {
+		fmt.Fprintf(&b, "%s-%s", Scores[scoreLeft], Scores[scoreRight])
+	}
+
+	return b.String()
 
 }
 
@@ -57,8 +80,10 @@ func (ls LiveSet) Render() string {
 	var b strings.Builder
 
 	fmt.Fprintf(&b, "%08b %08b %08b %08b\n", metaData, setScore, scoreLeft, scoreRight)
-	fmt.Fprintf(&b, "% 8d % 8d % 8d % 8d\n", metaData, setScore, scoreLeft, scoreRight)
+	fmt.Fprintf(&b, "% 8d  % 3d-% 3d % 8d % 8d\n", metaData, setScore>>4, setScore&0xF, scoreLeft, scoreRight)
 	fmt.Fprintf(&b, "Meta     Games    Score L  Score R\n\n")
+
+	fmt.Fprintln(&b, ls.Score())
 
 	return b.String()
 
